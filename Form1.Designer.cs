a86d663fdefc2ecf034589f1bd493786af015095
Leonardo Marques <ledomarques@gmail.com>
2021-02-12 11:49:11 -0300

    Pequenos ajustes nos textos explicativos.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -76,9 +76,9 @@ namespace ConverterPDF3
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(15, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(270, 17);
+            this.label2.Size = new System.Drawing.Size(265, 17);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Converta arquivos PDF para .DOCX!";
+            this.label2.Text = "Converta arquivos PDF para DOCX!";
             // 
             // btnConverte
             // 
